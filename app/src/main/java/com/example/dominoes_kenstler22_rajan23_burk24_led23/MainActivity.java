5afe3c90f46f6847e092b35d54ab9b39184d45f6
Kenstler <kenstler22@up.edu>
2021-09-16 18:36:03 -0700

    Initial commit

---
+++ app/src/main/java/com/example/dominoes_kenstler22_rajan23_burk24_led23/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.dominoes_kenstler22_rajan23_burk24_led23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
